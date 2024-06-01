0a7c2181ad3d8abad31429c540d150d8a92a1a84
Ekpodo1 <98796101+Ekpodo1@users.noreply.github.com>
2024-06-01 02:24:54 +0100

    Update wp-config.php

--- database-config/wp-config.php
+++ database-config/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'admin' );
 
 /** Database hostname */
-define( 'DB_HOST', '34.67.198.40' );
+define( 'DB_HOST', '104.154.172.38' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
